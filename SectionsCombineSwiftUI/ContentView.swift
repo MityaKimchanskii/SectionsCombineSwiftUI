5d0ea48ac550e0d780731cf94600dd2c3b2cf0e0
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-21 15:04:44 -0700

    Initial Commit

---
+++ SectionsCombineSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SectionsCombineSwiftUI
+//
+//  Created by Mitya Kim on 7/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SectionsCombineSwiftUI/SectionsCombineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SectionsCombineSwiftUIApp.swift
+//  SectionsCombineSwiftUI
+//
+//  Created by Mitya Kim on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SectionsCombineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
